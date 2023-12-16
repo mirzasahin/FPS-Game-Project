a453c1bd7861343eb47c6f057b94cfabec0cb128
Mirza Sahin <salihmirza123@gmail.com>
2023-12-16 17:35:02 +0300

    Add mouse movement actions

---
+++ Assets/Scripts/MouseMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour
+{
+
+    public float mouseSensivity = 100f;
+
+    float xRotation = 0f;
+    float yRotation = 0f;
+
+    public float topClamp = -90f;
+    public float bottomClamp = 90f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Locking the cursor to the middle of the screen and making it invisible
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Getting the mouse inputs
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensivity * Time.deltaTime;
+        Debug.Log(mouseX);
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensivity * Time.deltaTime;
+
+        // Rotation around the x axis (Look up and down)
+        xRotation -= mouseY;
+
+        // Clamp the rotation
+        xRotation = Mathf.Clamp(xRotation, topClamp, bottomClamp);
+        
+        // Rotation around the y axis (Look left and right)
+        yRotation += mouseX;
+
+        // Apply rotations to our transform
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+    }
+}
